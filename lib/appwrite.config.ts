045f9f7d7d5d2705f1cfff8ebe8f17631c107ba7
auyaroglu <ardauyaroglu@hotmail.com>
2024-07-14 20:25:39 +0300

    Appointment list,actions and designs have been implemented. Bugs fixed

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,4 +1,4 @@
-import * as sdk from "node-appwrite";
+import * as sdk from 'node-appwrite'
 
 export const {
     NEXT_PUBLIC_ENDPOINT: ENDPOINT,
@@ -9,17 +9,17 @@ export const {
     DOCTOR_COLLECTION_ID,
     APPOINTMENT_COLLECTION_ID,
     NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
-} = process.env;
+} = process.env
 
-
-const client = new sdk.Client();
+const client = new sdk.Client()
 client
-    .setEndpoint("https://cloud.appwrite.io/v1")
-    .setProject("668bb517001bff2f1a7c")
-    .setKey("e714c33fb7bc0fb650ba5539f9bc8dc91ce3904592c079c0625d087a9ed1b0b719726a1f188cbf285693ece888c7e41735f44abd29ddcd5239321a79fa517e484e61994c9c7a51e5c970b50f0acba183c0c1bad73d19dccc6108084887494a717aabebf1f13f618fe44632d2d75b17f1ed946ef06a1e082188774714727e1255");
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('668bb517001bff2f1a7c')
+    .setKey(
+        'e714c33fb7bc0fb650ba5539f9bc8dc91ce3904592c079c0625d087a9ed1b0b719726a1f188cbf285693ece888c7e41735f44abd29ddcd5239321a79fa517e484e61994c9c7a51e5c970b50f0acba183c0c1bad73d19dccc6108084887494a717aabebf1f13f618fe44632d2d75b17f1ed946ef06a1e082188774714727e1255',
+    )
 
-
-export const databases = new sdk.Databases(client);
-export const users = new sdk.Users(client);
-export const messaging = new sdk.Messaging(client);
-export const storage = new sdk.Storage(client);
+export const databases = new sdk.Databases(client)
+export const users = new sdk.Users(client)
+export const messaging = new sdk.Messaging(client)
+export const storage = new sdk.Storage(client)
